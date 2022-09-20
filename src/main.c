a615b742b66dc564b225bcf8a87d8bdc57fb6923
awsmit07 <awsmit@shaw.ca>
2022-09-20 08:29:15 -0600

    Configure PIC
    Configure PICs oscillator, programming port, TRISA, AD1PCFG register and
    TRISB register.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,19 @@
 
 #include "xc.h"
 
+// Disable the primary oscillator to use RA3 as an input
+#pragma config POSCMOD = NONE
+#pragma config OSCIOFNC = ON
+
+// Set the PGx3 port as the programmer
+#pragma config ICS = PGx3 
+
 int main(void) 
 {
+    // Set PORT A<3:0> as inputs and PORT B as output
+    TRISA = 0x000f;
+    AD1PCFG = 0xDC3F; // Configure all AN pins as digital inputs for use on hardware
+    TRISB = 0x0000;
+    
     return 0;
 }
